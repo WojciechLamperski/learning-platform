d8717d799c0635c726988e0c148b2067a201891b
Wojciech Lamperski <wolamp20@gmail.com>
2023-07-02 17:55:55 +0200

    wip: changing paths of tests

--- learning-platform/cypress.config.js
+++ learning-platform/cypress.config.js
@@ -2,9 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000/',
+    supportFile: false,
+    specPattern: '../e2e/*.e2e.js',
   },
 
   component: {
--- learning-platform/tests/e2e/spec.cy.js
+++ learning-platform/tests/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('localhost:3000');
+    cy.visit('/');
     cy.get('a').click();
     cy.get('a').click();
   })
