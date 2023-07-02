1c111a16b571bb5f8d5e3c64bd38101b837cc689
Wojciech Lamperski <wolamp20@gmail.com>
2023-07-02 22:52:32 +0200

    wip

--- learning-platform/cypress.config.js
+++ learning-platform/cypress.config.js
@@ -1,16 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  fixturesFolder: false,
+  screenshotOnRunFailure: false,
+  trashAssetsBeforeRuns: true,
+  video: false,
+  videoUploadOnPasses: false,
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'http://localhost:3000/',
     supportFile: false,
-    specPattern: '../e2e/*.e2e.js',
+    specPattern: 'tests/e2e/**/*.e2e.js',
   },
 
   component: {
+    specPattern: '**/*.test.js',
+    supportFile: 'tests/support/component.js',
+    indexHtmlFile: 'tests/support/component-index.html',
+    //czemu tego nie potrzebowaliśmy?
     devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
+      framework: 'next',
+      bundler: 'webpack',
+    }, 
   },
 });
